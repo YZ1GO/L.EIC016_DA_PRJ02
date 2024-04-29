9f2190ff0205b26b1a8c9bd234584649149d1e77
Ricardo Yang <ricardo.yang00@gmail.com>
2024-04-29 13:04:01 +0100

    implemented template for data parser

--- include/DataParser.h
+++ include/DataParser.h
@@ -5,20 +5,56 @@
 #include "Location.h"
 #include "Utilities.h"
 #include <fstream>
+#include <vector>
 
 class DataParser {
 private:
-    Graph<Location> bigGraph;
-
+    Graph<Location> RealGraph1;
+    Graph<Location> RealGraph2;
+    Graph<Location> RealGraph3;
+    Graph<Location> ExtraFullyConnectedGraph25;
+    Graph<Location> ExtraFullyConnectedGraph50;
+    Graph<Location> ExtraFullyConnectedGraph75;
+    Graph<Location> ExtraFullyConnectedGraph100;
+    Graph<Location> ExtraFullyConnectedGraph200;
+    Graph<Location> ExtraFullyConnectedGraph300;
+    Graph<Location> ExtraFullyConnectedGraph400;
+    Graph<Location> ExtraFullyConnectedGraph500;
+    Graph<Location> ExtraFullyConnectedGraph600;
+    Graph<Location> ExtraFullyConnectedGraph700;
+    Graph<Location> ExtraFullyConnectedGraph800;
+    Graph<Location> ExtraFullyConnectedGraph900;
     Graph<int> toyGraphShipping;
     Graph<int> toyGraphStadiums;
     Graph<int> toyGraphTourism;
 
+    void ParseToyGraphs(std::string shippingPath, std::string stadiumsPath, std::string tourismPath);
+    void ParseRealWorldGraphs(std::vector<std::string> datasetPaths);
+    void ParseExtraFullyConnectedGraphs(std::vector<std::string> datasetPaths);
+    void ParseBigGraph(std::string datasetPath);
 
 public:
     DataParser();
-    void ParseToysGraph(std::string shippingPath, std::string stadiumsPath, std::string tourismPath);
-
+    DataParser(std::string shippingPath, std::string stadiumsPath, std::string tourismPath,
+               std::vector<std::string> extraFullyConnectedPaths, std::vector<std::string> realWorldPaths);
+    Graph<Location> getRealGraph1;
+    Graph<Location> getRealGraph2;
+    Graph<Location> getRealGraph3;
+    Graph<Location> getExtraFullyConnectedGraph25;
+    Graph<Location> getExtraFullyConnectedGraph50;
+    Graph<Location> getExtraFullyConnectedGraph75;
+    Graph<Location> getExtraFullyConnectedGraph100;
+    Graph<Location> getExtraFullyConnectedGraph200;
+    Graph<Location> getExtraFullyConnectedGraph300;
+    Graph<Location> getExtraFullyConnectedGraph400;
+    Graph<Location> getExtraFullyConnectedGraph500;
+    Graph<Location> getExtraFullyConnectedGraph600;
+    Graph<Location> getExtraFullyConnectedGraph700;
+    Graph<Location> getExtraFullyConnectedGraph800;
+    Graph<Location> getExtraFullyConnectedGraph900;
+    Graph<int> gettoyGraphShipping;
+    Graph<int> gettoyGraphStadiums;
+    Graph<int> gettoyGraphTourism;
 };
 
 
